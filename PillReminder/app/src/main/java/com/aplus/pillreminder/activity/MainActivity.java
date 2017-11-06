409b8ca67fd104b5ef88bdf46fdc46e49d0b0031
Weerabhat Meelarp <s8070128@kmitl.ac.th>
2017-11-06 18:20:56 +0700

    Connect to HomeFragment to AddPillFragment

--- PillReminder/app/src/main/java/com/aplus/pillreminder/activity/MainActivity.java
+++ PillReminder/app/src/main/java/com/aplus/pillreminder/activity/MainActivity.java
@@ -1,12 +1,14 @@
 package com.aplus.pillreminder.activity;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.aplus.pillreminder.R;
+import com.aplus.pillreminder.fragment.AddPillFragment;
 import com.aplus.pillreminder.fragment.HomeFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomeFragment.HomeFragmentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragmentContainer, new HomeFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public void onBtnAddPress() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(R.id.fragmentContainer, new AddPillFragment())
+                .addToBackStack(null)
+                .commit();
     }
 }
--- PillReminder/app/src/main/java/com/aplus/pillreminder/fragment/HomeFragment.java
+++ PillReminder/app/src/main/java/com/aplus/pillreminder/fragment/HomeFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.aplus.pillreminder.R;
 
@@ -13,8 +14,14 @@ import com.aplus.pillreminder.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
+    private Button btnAdd;
+    private HomeFragmentListener listener;
+
+    public interface HomeFragmentListener {
+        void onBtnAddPress();
+    }
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,7 +32,30 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        setup(view);
+
+        return view;
     }
 
+    private void setup(View view) {
+        listener = (HomeFragmentListener) getActivity();
+
+        btnAdd = view.findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                onBtnAdd();
+                break;
+        }
+    }
+
+    private void onBtnAdd() {
+        listener.onBtnAddPress();
+    }
 }
