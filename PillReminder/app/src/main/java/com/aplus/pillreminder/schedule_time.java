4d6ee1e84f5393291e66423337f4edd3595a83a4
zeanityza <te199@hotmail.com>
2017-11-14 21:55:43 +0700

    add_and_change
    addbottombar,addMainFragment,Change_pill,add_pic

---
+++ PillReminder/app/src/main/java/com/aplus/pillreminder/schedule_time.java
@@ -0,0 +1,13 @@
+package com.aplus.pillreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class schedule_time extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_time);
+    }
+}
