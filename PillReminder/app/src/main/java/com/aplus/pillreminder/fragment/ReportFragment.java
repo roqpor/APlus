a3bbe96198e32ab14677c1d72ca9f6264c7802d1
tiwipabmin <tiwipabmin@gmail.com>
2017-12-12 01:08:32 +0700

    test create tree view finished.

--- PillReminder/app/src/main/java/com/aplus/pillreminder/fragment/ReportFragment.java
+++ PillReminder/app/src/main/java/com/aplus/pillreminder/fragment/ReportFragment.java
@@ -5,8 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.aplus.pillreminder.R;
+import com.unnamed.b.atv.model.TreeNode;
+import com.unnamed.b.atv.view.AndroidTreeView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +20,7 @@ import com.aplus.pillreminder.R;
 public class ReportFragment extends Fragment {
 
     public static String TAG = ReportFragment.class.getSimpleName();
+    @BindView(R.id.container_report) FrameLayout container_report;
 
     public ReportFragment() {
         // Required empty public constructor
@@ -23,8 +30,18 @@ public class ReportFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_report, container, false);
+        ButterKnife.bind(this, rootView);
+        TreeNode root = TreeNode.root();
+        TreeNode parent = new TreeNode("MyParentNode");
+        TreeNode child1 = new TreeNode("Ch1");
+        TreeNode child2 = new TreeNode("Ch2");
+        parent.addChildren(child1, child2);
+        root.addChild(parent);
+
+        AndroidTreeView tView = new AndroidTreeView(getActivity(), root);
+        container_report.addView(tView.getView());
+        return rootView;
     }
 
 }
